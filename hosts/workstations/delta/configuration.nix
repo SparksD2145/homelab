8875576cb76ac500005145a161ebb604810015ab
Thomas Ibarra <hello@iwrite.software>
2026-05-21 14:39:53 -0500

    delta: disable niri and switch back to gnome

--- hosts/workstations/delta/configuration.nix
+++ hosts/workstations/delta/configuration.nix
@@ -26,10 +26,6 @@
   # Install firefox.
   programs.firefox.enable = true;
 
-  environment.systemPackages = [
-    pkgs.nemo
-  ];
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
