a18445ad7712a5c369969cb744e94ac1a9c39afe
Thomas Ibarra <hello@iwrite.software>
2026-06-17 09:11:10 -0500

    claude-code: attempt ollama alias

--- hosts/modules/users/sparks/shell.nix
+++ hosts/modules/users/sparks/shell.nix
@@ -46,7 +46,7 @@
         code = "codium";
         update = "sudo nixos-rebuild switch --flake 'github:SparksD2145/homelab'";
         fluxupdate = "git add .; git commit --amend --no-edit; git push -f; git push gitlab -f; flux reconcile kustomization flux-system --with-source;";
-
+        ollama-claude = "ANTHROPIC_AUTH_TOKEN=ollama ANTHROPIC_BASE_URL='https://ollama.sparks.codes' claude --model 'gemma4:26b-a4b-it-qat'"
       };
       history.size = 10000;
 
