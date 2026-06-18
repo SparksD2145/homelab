d4212376dff729fa874e28b7cd48ee6be88fefba
Thomas Ibarra <hello@iwrite.software>
2026-06-18 12:42:43 -0500

    nix-ld: enable dynamic libraries

--- hosts/modules/global/default.nix
+++ hosts/modules/global/default.nix
@@ -41,6 +41,9 @@
         };
       };
 
+      # Enable dynamic libraries
+      programs.nix-ld.enable = true;
+
       # Fuse filesystem that returns symlinks to executables based on the PATH of the requesting process. This is useful to execute shebangs on NixOS that assume hard coded locations in locations like /bin or /usr/bin etc.
       services.envfs.enable = true;
 
