684fdd42b7872f263c979275730fb8a591fc2444
Thomas Ibarra <hello@iwrite.software>
2026-06-24 21:52:13 -0500

    blaude: use deepseek-coder-v2:16b

--- hosts/modules/users/sparks/shell.nix
+++ hosts/modules/users/sparks/shell.nix
@@ -46,7 +46,7 @@
         code = "codium";
         update = "sudo nixos-rebuild switch --flake 'github:SparksD2145/homelab'";
         fluxupdate = "git add .; git commit --amend --no-edit; git push -f; git push gitlab -f; flux reconcile kustomization flux-system --with-source;";
-        blaude = "ANTHROPIC_BASE_URL='https://ai.sparks.codes' claude --model 'gemma4:26b-a4b-it-qat'";
+        blaude = "ANTHROPIC_BASE_URL='https://ai.sparks.codes' claude --model 'deepseek-coder-v2:16b'";
       };
       history.size = 10000;
 
