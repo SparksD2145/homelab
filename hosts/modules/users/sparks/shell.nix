5e477a102a1b03654a252adf2e64de99f2d673c3
Thomas Ibarra <hello@iwrite.software>
2026-07-22 08:35:46 -0500

    claude-code: remove

--- hosts/modules/users/sparks/shell.nix
+++ hosts/modules/users/sparks/shell.nix
@@ -46,7 +46,6 @@
         code = "codium";
         update = "sudo nixos-rebuild switch --flake 'github:SparksD2145/homelab'";
         fluxupdate = "git add .; git commit --amend --no-edit; git push -f; git push gitlab -f; flux reconcile kustomization flux-system --with-source;";
-        blaude = "ANTHROPIC_BASE_URL='https://local-ai.sparks.codes' claude --model 'gemma4:26b-a4b-it-qat'";
       };
       history.size = 10000;
 
@@ -65,11 +64,6 @@
       initContent = ''
         export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin";
         export PATH="$PATH:$HOME/.krew/bin"
-
-        # Export Anthropic Token
-        export ANTHROPIC_AUTH_TOKEN="$(jq -r '.localai.key' ${
-          config.sops.secrets."users/sparks/opencode/auth".path
-        })"
       '';
     };
   };
