3846192f0e50a65fc7bec882cce83bfe95c19ce3
Thomas Ibarra <hello@iwrite.software>
2026-08-23 19:17:51 -0500

    beta(nvidia): add power limits for compute gpus

--- hosts/servers/beta/configuration.nix
+++ hosts/servers/beta/configuration.nix
@@ -22,6 +22,32 @@
   hardware.nvidia.open = lib.mkForce false;
   hardware.nvidia.package = lib.mkForce config.boot.kernelPackages.nvidiaPackages.dc;
 
+  # Set nvidia power limits at boot
+  systemd.services.my-boot-script = {
+    description = "My custom system boot script";
+
+    # Ensure the service starts late in the boot sequence
+    wantedBy = [ "multi-user.target" ];
+
+    # Inject dependencies into the script's PATH environment variable
+    path = [
+      pkgs.coreutils
+      pkgs.bash
+    ];
+
+    # The exact script commands you want to execute
+    script = ''
+      # Set power limits on bootup.
+      nvidia-smi -i 0 -pl 150
+      nvidia-smi -i 1 -pl 175
+    '';
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
