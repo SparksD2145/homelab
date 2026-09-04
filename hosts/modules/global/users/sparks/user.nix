6dcbfe10215225b6d180436a4bddf1de8e217d7f
Thomas Ibarra <hello@iwrite.software>
2026-09-04 11:15:59 -0500

    user(sparks): remove check for niri

--- hosts/modules/global/users/sparks/user.nix
+++ hosts/modules/global/users/sparks/user.nix
@@ -56,7 +56,7 @@
     home.packages = builtins.concatLists [
       (import ./packages.nix { inherit inputs pkgs; })
       (
-        if config.services.xserver.enable == true || config.programs.niri.enable == true then
+        if config.services.xserver.enable == true then
           import ./packages-gui.nix { inherit inputs pkgs; }
         else
           [ ]
